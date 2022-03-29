e002b13bed1b7406a65f01605bfbcf75b0d79056
hlbotterman <hlbotterman@quantmetry.com>
2022-03-29 09:19:17 +0000

    print rank

--- robust_pca/utils/utils.py
+++ robust_pca/utils/utils.py
@@ -71,7 +71,8 @@ def approx_rank(M: np.ndarray, th: Optional[float]=1) -> int:
     if th == 1:
         return min(M.shape)
     else:
-        print("sum in rank:", np.isnan(M).sum())
+        print("\n\ndimension M:", M.shape, "\n\n")
+        print("\n\nsum in rank:", np.isnan(M).sum(), "\n\n")
         _, s, _ = np.linalg.svd(M, full_matrices=True)
         nuclear = np.sum(s)
         cum_sum = np.cumsum([i / nuclear for i in s])
