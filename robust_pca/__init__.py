22332171de8f27bfd83983812759c3f2b9527cc6
vm-aifluence-jro <ec2-user@ip-10-0-0-95.eu-west-3.compute.internal>
2022-05-23 09:25:39 +0000

    folder classes deleted

--- robust_pca/__init__.py
+++ robust_pca/__init__.py
@@ -1,6 +1,5 @@
 from ._version import __version__
 
-from . import classes
 from . import utils
 
-__all__ = ["classes", "utils", "__version__"]
+__all__ = ["utils", "__version__"]
--- robust_pca/classes/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import rpca
-from . import improved_rpca
-from . import noisy_rpca
-from . import graph_rpca
