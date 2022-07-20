42fef166f6d9b4456cd351ad7512b6430ef6272b
Hong-Lan Botterman <hlbotterman@quantmetry.com>
2022-07-20 14:28:20 +0200

    modify get_data with download choice

--- qolmat/utils/data.py
+++ qolmat/utils/data.py
@@ -4,45 +4,50 @@ import sys
 import pandas as pd
 import numpy as np
 import zipfile
+from datetime import datetime
 
-def get_data(datapath="data/"):
-    urllink = "https://archive.ics.uci.edu/ml/machine-learning-databases/00501/"
-    # datapath = "../data/"
-    zipname = "PRSA2017_Data_20130301-20170228"
-    path_zip = os.path.join(datapath, zipname)
-    
-    if not os.path.exists(path_zip + ".zip"):
-        urllib.request.urlretrieve(urllink+zipname+".zip", path_zip + ".zip")
-    with zipfile.ZipFile(path_zip + ".zip", "r") as zip_ref:
-        zip_ref.extractall(path_zip)
-    data_folder = os.listdir(path_zip)
-    subfolder = os.path.join(path_zip, data_folder[0])
-    data_files = os.listdir(subfolder)
-    # cities = sorted([file.split("_")[2] for file in data_files])
-    # dfs_init = {city: pd.read_csv(os.path.join(subfolder, file)) for city, file in zip(cities, data_files)}
-    # dfs_init = {city: preprocess_data(df) for city, df in dfs_init.items()}
-    # df = pd.concat(dfs_init.values(), keys=dfs_init.keys())
-    list_df = [pd.read_csv(os.path.join(subfolder, file)) for file in data_files]
-    list_df = [preprocess_data(df) for df in list_df]
-    df = pd.concat(list_df)
-    return df
-    # for city, df in dfs_init.items():
-    #     df.index = pd.to_datetime(df[["year", "month", "day", "hour"]])
-    #     df = df.interpolate().ffill().bfill()
-    # df_init = pd.concat(dfs_init)
-    # cols = ["PM2.5", "PM10", "SO2", "NO2", "CO", "O3", "TEMP", "PRES", "DEWP", "RAIN"]
-    # cols_sum = ["RAIN"]
-    # cols_mean = list(set(cols) - set(cols_sum))
-    # cols_imp = ["TEMP", "PRES", "DEWP"]
-    # cols_time = ["year", "month", "day", "hour"]
-    # df_init_day = pd.concat([
-    #     df_init.groupby(["station", "year", "month", "day"])[cols_mean].mean(),
-    #     df_init.groupby(["station", "year", "month", "day"])[cols_sum].sum(),
-    # ])
-    # df_init_day["date"] = pd.to_datetime(df_init_day.reset_index()[["year", "month", "day"]]).values
-    # # df_init_day["station"] = df_init_day.index.get_level_values(0)
-    # df_init_day = df_init_day.reset_index().set_index(["station", "date"], drop=False)
-    # return dfs_init
+
+def get_data(datapath: str = "data/", download: bool = True):
+    """Download or generate data
+
+    Parameters
+    ----------
+    datapath : str, optional
+        data path, by default "data/"
+    download : bool, optional
+        if True: download a public dataset, if False: generate random univariate time series, by default True
+
+    Returns
+    -------
+    pd.DataFrame
+        requested data
+    """
+    if download:
+        urllink = "https://archive.ics.uci.edu/ml/machine-learning-databases/00501/"
+        zipname = "PRSA2017_Data_20130301-20170228"
+        path_zip = os.path.join(datapath, zipname)
+
+        if not os.path.exists(path_zip + ".zip"):
+            urllib.request.urlretrieve(urllink + zipname + ".zip", path_zip + ".zip")
+        with zipfile.ZipFile(path_zip + ".zip", "r") as zip_ref:
+            zip_ref.extractall(path_zip)
+        data_folder = os.listdir(path_zip)
+        subfolder = os.path.join(path_zip, data_folder[0])
+        data_files = os.listdir(subfolder)
+        list_df = [pd.read_csv(os.path.join(subfolder, file)) for file in data_files]
+        list_df = [preprocess_data(df) for df in list_df]
+        df = pd.concat(list_df)
+        return df
+    else:
+        city = "Wonderland"
+        x = np.linspace(0, 4 * np.pi, 200)
+        y = 3 + np.sin(x) + np.random.random(len(x)) * 0.2
+        datelist = pd.date_range(datetime(2013, 3, 1), periods=len(y)).tolist()
+        dataset = pd.DataFrame(
+            {"var": y, "datetime": datelist[: len(y)], "station": city}
+        )
+        dataset.set_index(["station", "datetime"], inplace=True)
+        return dataset
 
 
 def preprocess_data(df):
@@ -56,5 +61,7 @@ def preprocess_data(df):
     # df[cols_sum] = df.groupby(["station", df.index.get_level_values("datetime").date]).apply(np.sum)
     dict_agg = {key: np.mean for key in df.columns}
     dict_agg["RAIN"] = np.mean
-    df = df.groupby(["station", df.index.get_level_values("datetime").floor("d")]).agg(dict_agg)
+    df = df.groupby(["station", df.index.get_level_values("datetime").floor("d")]).agg(
+        dict_agg
+    )
     return df
