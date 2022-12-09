ee1134df18437fc1b38627efa5beef7b39979495
Hông-Lan Botterman <hlbotterman@quantmetry.com>
2022-12-09 16:30:51 +0100

    ENH: test imputation models

--- qolmat/tests/test_models.py
+++ qolmat/tests/test_models.py
@@ -4,6 +4,8 @@ import pytest
 import pandas as pd
 import numpy as np
 from qolmat.imputations import models
+from qolmat.utils import missing_patterns
+from qolmat.benchmark import utils
 
 
 def test_impute_by_mean_fit_transform() -> None:
@@ -97,7 +99,7 @@ def test_impute_linear_interpolation_fit_transform() -> None:
     )
 
 
-def test_impute_linear_interpolation_fit_transform() -> None:
+def test_impute_KNN_fit_transform() -> None:
     test_imputer = models.ImputeKNN(k=2)
     res = test_imputer.fit_transform(
         pd.DataFrame(
@@ -111,15 +113,61 @@ def test_impute_linear_interpolation_fit_transform() -> None:
     )
 
 
-def test_impute_linear_interpolation_fit_transform() -> None:
+def test_impute_KNN_hyperparameters() -> None:
+
     test_imputer = models.ImputeKNN(k=2)
-    res = test_imputer.fit_transform(
-        pd.DataFrame(
-            [[1, 1, 1, 1], [np.nan, np.nan, np.nan, np.nan], [1, 2, 2, 5], [2, 2, 2, 2]]
-        )
+
+    assert len(test_imputer.get_hyperparams()) == 1
+    assert test_imputer.get_hyperparams() == {"k": 2}
+
+
+def test_impute_MICE_fit_transform() -> None:
+
+    from sklearn.ensemble import ExtraTreesRegressor
+
+    n, p = 100, 4
+    mu = np.random.rand(p)
+    A = np.random.rand(p, p)
+    sigma = np.dot(A, A.transpose())
+    df_init = np.random.multivariate_normal(mu, sigma, n)
+    df_init = pd.DataFrame(data=df_init)
+
+    df_corrupted = df_init.copy()
+    X_miss_mcar = missing_patterns.produce_NA(df_corrupted, p_miss=0.2, mecha="MCAR")
+    df_corrupted = X_miss_mcar["X_incomp"]
+    mask_mcar = X_miss_mcar["mask"]
+
+    test_imputer = models.ImputeMICE(
+        estimator=ExtraTreesRegressor(),
+        sample_posterior=False,
+        max_iter=100,
+        missing_values=np.nan,
     )
 
-    np.testing.assert_array_equal(
-        res,
-        [[1, 1, 1, 1], [4 / 3, 10 / 6, 10 / 6, 16 / 6], [1, 2, 2, 5], [2, 2, 2, 2]],
+    res_mice = test_imputer.fit_transform(df_corrupted)
+    rmse_mice = utils.mean_absolute_error(
+        df_init[mask_mcar], res_mice[mask_mcar], columnwise=False
     )
+
+    test_imputer = models.ImputeRandom()
+    res_random = test_imputer.fit_transform(df_corrupted)
+    rmse_random = utils.mean_absolute_error(
+        df_init[mask_mcar], res_random[mask_mcar], columnwise=False
+    )
+
+    assert rmse_mice < rmse_random
+
+
+def test_impute_MICE_hyperparameters() -> None:
+
+    from sklearn.ensemble import ExtraTreesRegressor
+
+    test_imputer = models.ImputeMICE(
+        estimator=ExtraTreesRegressor(),
+        sample_posterior=False,
+        max_iter=100,
+        missing_values=np.nan,
+    )
+
+    assert len(test_imputer.get_hyperparams()) == 4
+    assert "estimator" in test_imputer.get_hyperparams()
