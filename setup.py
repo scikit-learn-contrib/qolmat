e7adaa2d11a7fbe0d52b0ef5ff087fcaad5b3815
Dakhli_Firas <fdakhli@quantmetry.com>
2023-02-24 18:04:05 +0100

    Updating Readme in steup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DISTNAME = "qolmat"
 VERSION = "0.0.2"
 DESCRIPTION = "Tools to impute"
 LONG_DESCRIPTION_CONTENT_TYPE = "text/x-rst"
-with codecs.open("NEW_README.rst", encoding="utf-8-sig") as f:
+with codecs.open("README.rst", encoding="utf-8-sig") as f:
     LONG_DESCRIPTION = f.read()
 # LONG_DESCRIPTION = "hello"
 
