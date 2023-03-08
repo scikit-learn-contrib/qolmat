118d8d86980af11d2287494d60cbb168fdb21af5
Firas <fdakhli@quantmetry.com>
2023-03-08 12:59:49 +0100

    upgrading version and benchmark

--- qolmat/__init__.py
+++ qolmat/__init__.py
@@ -1,4 +1,4 @@
 from . import benchmark, imputations, utils
 from ._version import __version__
 
-__all__ = ["utils", "__version__", "benchmark", "imputations"]
+__all__ = ["benchmark", "imputations", "utils", "__version__"]
