dc8b29e09fc201b042d52a3213f21a40c50623b5
Anh-Khoa NGO-HO <angoho@quantmetry.com>
2023-06-05 09:51:02 +0200

    feat: add unit tests for missing_patterns

--- tests/benchmark/test_missing_patterns.py
+++ tests/benchmark/test_missing_patterns.py
@@ -26,3 +26,69 @@ def test_SamplerHoleGenerator_split(df: pd.DataFrame, generator: mp._HoleGenerat
     expected_col2_holes = 10
     np.testing.assert_allclose(col1_holes, expected_col1_holes)
     np.testing.assert_allclose(col2_holes, expected_col2_holes)
+
+
+df_group = pd.DataFrame(
+    {"col1": [i for i in range(100)] + [np.nan], "col2": [2 * i for i in range(100)] + [np.nan]},
+    index=np.concatenate([[i for i in range(33)] for j in range(3)] + [[0, 0]]),
+)
+
+
+@pytest.mark.parametrize(
+    "df, generator",
+    [
+        (
+            df_group,
+            mp.GroupedHoleGenerator(
+                n_splits=2, ratio_masked=0.1, random_state=42, groups=["group"]
+            ),
+        )
+    ],
+)
+def test_GroupedHoleGenerator_split(df: pd.DataFrame, generator: mp._HoleGenerator) -> None:
+    df.index = df.index.rename("group")
+
+    mask = generator.split(df)[0]
+
+    num_group = df.index.unique().size
+    holes_grouped = mask.groupby("group").sum()
+    num_groups_holes_col1 = len(holes_grouped[holes_grouped["col1"] > 1])
+    num_groups_holes_col2 = len(holes_grouped[holes_grouped["col2"] > 1])
+
+    ratio_col1 = num_groups_holes_col1 / num_group
+    ratio_col2 = num_groups_holes_col2 / num_group
+
+    expected_ratio_col1 = 0.1
+    expected_ratio_col2 = 0.1
+
+    np.testing.assert_allclose(ratio_col1, expected_ratio_col1, atol=0.05)
+    np.testing.assert_allclose(ratio_col2, expected_ratio_col2, atol=0.05)
+
+
+df_complet = pd.DataFrame({"col1": [i for i in range(100)], "col2": [2 * i for i in range(100)]})
+
+
+@pytest.mark.parametrize(
+    "df, generator",
+    [
+        (df_complet, list_generators["geo"]),
+        (df_complet, list_generators["unif"]),
+        (df_complet, list_generators["multi"]),
+    ],
+)
+def test_SamplerHoleGenerator_without_real_nans(
+    df: pd.DataFrame, generator: mp._HoleGenerator
+) -> None:
+    real_nan = np.random.choice([True, False], size=df.size, p=[0.2, 0.8]).reshape(100, 2)
+    df[real_nan] = np.nan
+
+    mask = generator.split(df)[0]
+
+    loc_real_nans_col1 = np.where(df["col1"].isna())[0]
+    loc_mask_col1 = np.where(mask["col1"] == True)[0]
+
+    loc_real_nans_col2 = np.where(df["col2"].isna())[0]
+    loc_mask_col2 = np.where(mask["col2"] == True)[0]
+
+    np.testing.assert_allclose(len(set(loc_real_nans_col1) & set(loc_mask_col1)), 0)
+    np.testing.assert_allclose(len(set(loc_real_nans_col2) & set(loc_mask_col2)), 0)
