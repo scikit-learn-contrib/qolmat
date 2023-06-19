f125692fcbef99990b7d0ea6bd304b99f8bd3d3e
Gsaes <gsaes@quantmetry.com>
2023-06-19 17:59:53 +0200

    Test Keras

--- tests/imputations/test_imputers_keras.py
+++ tests/imputations/test_imputers_keras.py
@@ -45,11 +45,12 @@ def test_ImputerRegressorKeras_fit_transform(df: pd.DataFrame) -> None:
     result = imputer.fit_transform(df)
     expected = pd.DataFrame(
         {
-            "col1": [38.36250, 15.0, 38.36526, 23.0, 33.0],
+            "col1": [38.362286, 15.0, 38.365032, 23.0, 33.0],
             "col2": [69.0, 76.0, 74.0, 80.0, 78.0],
             "col3": [174.0, 166.0, 182.0, 177.0, 38.36550],
             "col4": [9.0, 12.0, 11.0, 12.0, 8.0],
-            "col5": [93.0, 75.0, 38.36526, 12.0, 38.36547],
+            "col5": [93.0, 75.0, 38.365032, 12.0, 38.36547],
         }
     )
+
     np.testing.assert_allclose(result, expected, atol=1e-5)
