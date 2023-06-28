31892019754f7ed6e3c31b9ef7ac598d0958b262
Gsaes <gsaes@quantmetry.com>
2023-06-28 16:18:31 +0200

    rien

--- tests/benchmark/test_hyperparameters.py
+++ tests/benchmark/test_hyperparameters.py
@@ -120,4 +120,4 @@ def test_hyperparameters_optimize():
         imputer, df, generator, metric, dict_config_opti, max_evals=200
     )
     assert isinstance(imputer_opti, ImputerTest)
-    np.testing.assert_almost_equal(imputer_opti.value, 4, decimal=1)
+    np.testing.assert_almost_equal(imputer_opti.value, 4, decimal=2)
