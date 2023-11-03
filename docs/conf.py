cac3bd8aa4fbc07a64c8729e785c5390428dabd6
Julien Roussel <3178729-JulienRoussel77@users.noreply.gitlab.com>
2023-11-03 15:29:41 +0100

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = "2022, Quantmetry"
 author = "Quantmetry"
 
 # The full version, including alpha/beta/rc tags
-version = "0.1.0"
+version = "0.1.1"
 release = version
 
 # -- General configuration ---------------------------------------------------
