e2cf4ac82ac0ab27aa8e1c2f1a75112b2602309a
Anh-Khoa Ngo-Ho <angoho@quantmetry.com>
2023-11-23 15:23:09 +0000

    refactor

--- qolmat/benchmark/imputer_predictor.py
+++ qolmat/benchmark/imputer_predictor.py
@@ -1072,29 +1072,27 @@ def plot_scatter(
 def get_relative_score(x, df, col, method='gain'):
     # https://en.wikipedia.org/wiki/Relative_change
     if x['hole_generator'] == 'None':
-        x_row =  x[col]
+        x_row = x[col]
         x_ref = df[
-            (df['dataset']==x['dataset'])
-            & (df['n_fold']==x['n_fold'])
-            & (df['hole_generator']==x['hole_generator'])
-            & (df['ratio_masked']==x['ratio_masked'])
-            & (df['predictor']==x['predictor'])
-            & (df['imputer']=='None')
-            ][col]
+            (df['dataset'] == x['dataset'])
+            & (df['n_fold'] == x['n_fold'])
+            & (df['hole_generator'] == x['hole_generator'])
+            & (df['ratio_masked'] == x['ratio_masked'])
+            & (df['predictor'] == x['predictor'])
+            & (df['imputer'] == 'None')][col]
     else:
-        x_row =  x[col]
+        x_row = x[col]
         x_ref = df[
-            (df['dataset']==x['dataset']) 
-            & (df['n_fold']==x['n_fold'])
-            & (df['hole_generator']==x['hole_generator'])
-            & (df['ratio_masked']==x['ratio_masked'])
-            & (df['n_mask']==x['n_mask'])
-            & (df['predictor']==x['predictor'])
-            & (df['imputer']=='None')
-            ][col]
-    
+            (df['dataset'] == x['dataset'])
+            & (df['n_fold'] == x['n_fold'])
+            & (df['hole_generator'] == x['hole_generator'])
+            & (df['ratio_masked'] == x['ratio_masked'])
+            & (df['n_mask'] == x['n_mask'])
+            & (df['predictor'] == x['predictor'])
+            & (df['imputer'] == 'None')][col]
+
     if method == 'relative_percentage_gain':
-        x_out = ((x_ref - x_row) * 100)/x_ref
+        x_out = ((x_ref - x_row) * 100) / x_ref
     elif method == 'gain':
         x_out = x_ref - x_row
     else:
