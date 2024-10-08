b8308740f771160dcf077993ec636e7cf7182f65
Hông-Lan Botterman <hlbotterman@quantmetry.com>
2024-10-08 15:15:26 +0200

    execute code in rst files and improve unique workflow for unit tests and build docs

--- tests/conftest.py
+++ tests/conftest.py
@@ -62,8 +62,7 @@ def pytest_sessionstart(session: Any) -> None:
     """Run tests (hook) on specified .rst files at pytest session start.
 
     This function reads through a list of predefined .rst files,
-    extracts Python code blocks, and ensures that the code is syntactically
-    valid and that all necessary imports work correctly.
+    extracts Python code blocks, concatenates them, and executes the code.
     The function will scan each file listed in `RST_FILES_TO_TEST`.
 
     This function is invoked automatically by pytest when the session starts.
@@ -90,39 +89,23 @@ def pytest_sessionstart(session: Any) -> None:
             with open(rst_file, encoding="utf-8") as f:
                 content = f.read()
             code_blocks = extract_python_blocks(content)
+            code = "\n".join(code_blocks)
 
-            for i, code_block in enumerate(code_blocks):
+            if code:
                 try:
-                    tree = ast.parse(code_block)
-                    for node in ast.walk(tree):
-                        if isinstance(node, ast.Import):
-                            for name in node.names:
-                                try:
-                                    __import__(name.name)
-                                except ImportError as e:
-                                    pytest.fail(
-                                        f"Cannot import {name.name} in "
-                                        f"{rst_file}: {str(e)}"
-                                    )
-                        elif isinstance(node, ast.ImportFrom):
-                            if node.module:
-                                try:
-                                    __import__(node.module)  # noqa
-                                except ImportError as e:
-                                    pytest.fail(
-                                        f"Cannot import {node.module} in "
-                                        f"{rst_file}: {str(e)}."
-                                    )
-                            else:
-                                pytest.fail(
-                                    f"Module name is None in {rst_file} "
-                                    "in ImportFrom statement."
-                                )
+                    tree = ast.parse(code)
+                    exec(code, globals(), locals())
                 except SyntaxError as e:
                     pytest.fail(
-                        "Invalid Python syntax in code block "
-                        f"{i + 1} in {rst_file}: "
-                        f"\n{code_block}\nError: {str(e)}"
+                        "Syntax error in code block "
+                        f"in {rst_file}: \n{code}\nError: {str(e)}."
                     )
+                except Exception as e:
+                    pytest.fail(
+                        f"Error while executing the code in {rst_file}: "
+                        f"{str(e)}."
+                    )
+            else:
+                logging.info(f"No Python code blocks found in {rst_file}.")
         else:
-            logging.info(f"File {rst_file} does not exist, skippin...")
+            logging.info(f"File {rst_file} does not exist, skipping.")
