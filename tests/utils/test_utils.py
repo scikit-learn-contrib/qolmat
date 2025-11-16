83672b9edc0a83063f23a9abfd3c98fd885398a1
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-15 20:38:01 -0500

    spelling: consistent
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -143,7 +143,7 @@ def test_utils_prepare_data_2D_uneven(X: NDArray):
 
 
 @pytest.mark.parametrize("X", [X_incomplete])
-def test_utils_prepare_data_consistant(X: NDArray):
+def test_utils_prepare_data_consistent(X: NDArray):
     result1 = utils.prepare_data(X, 1)
     result2 = utils.prepare_data(result1, 2)
     result3 = utils.prepare_data(X, 2)
