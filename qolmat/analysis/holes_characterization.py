3620bc9c9b14b1cf34d3e095c8fdb935e840176c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-15 20:05:30 -0500

    spelling: across
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- qolmat/analysis/holes_characterization.py
+++ qolmat/analysis/holes_characterization.py
@@ -70,7 +70,7 @@ class LittleTest(McarTest):
     """Little Test class.
 
     This class implements the Little's test, which is designed to detect the
-    heterogeneity accross the missing patterns. The null hypothesis is
+    heterogeneity across the missing patterns. The null hypothesis is
     "The missing data mechanism is MCAR". The shortcoming of this test is
     that it won't detect the heterogeneity of covariance.
 
